3095e59c23e7a050a12143bfb3a47b36cedc94fb
KiritoU <nguyentienduongtink21@gmail.com>
2023-11-13 17:15:10 +0700

    first done

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ TOKEN = env.get("TOKEN")
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
